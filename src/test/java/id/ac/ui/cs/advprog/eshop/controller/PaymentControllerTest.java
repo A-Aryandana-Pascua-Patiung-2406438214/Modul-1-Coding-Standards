a99a3b100aee74c14bc4e1fa9ad47c4e8620f0be
Aryandana Pascua Patiung <nandapascua@Aryandanas-MacBook-Pro.local>
2026-03-10 19:51:58 +0700

    [RED] Implement functional tests for Payment feature

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/PaymentControllerTest.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PaymentController.class)
+class PaymentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PaymentService paymentService;
+
+    @Test
+    void testPaymentAdminListPage() throws Exception {
+        mockMvc.perform(get("/payment/admin/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payment/admin/list"));
+    }
+
+    @Test
+    void testPaymentDetailPage() throws Exception {
+        mockMvc.perform(get("/payment/detail"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payment/detail"));
+    }
+}
