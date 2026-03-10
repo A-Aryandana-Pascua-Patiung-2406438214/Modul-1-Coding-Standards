99a7286bbd6060221358bd6fe142e1f18c5f27dd
Aryandana Pascua Patiung <nandapascua@Aryandanas-MacBook-Pro.local>
2026-03-10 19:50:41 +0700

    [GREEN] Implement OrderController to pass functional tests

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/create")
+    public String createOrderPage(Model model) {
+        return "order/create";
+    }
+
+    @GetMapping("/history")
+    public String historyPage() {
+        return "order/history";
+    }
+
+    @PostMapping("/history")
+    public String orderHistory(@RequestParam String author, Model model) {
+        List<Order> orders = orderService.findAllByAuthor(author);
+        model.addAttribute("orders", orders);
+        model.addAttribute("author", author);
+        return "order/list";
+    }
+
+    @GetMapping("/pay/{orderId}")
+    public String payOrderPage(@PathVariable String orderId, Model model) {
+        Order order = orderService.findById(orderId);
+        model.addAttribute("order", order);
+        return "order/pay";
+    }
+}
