908b1b185bc34fcbef8645f94b70e9c7a4ba1dd4
Aryandana Pascua Patiung <nandapascua@Aryandanas-MacBook-Pro.local>
2026-03-10 18:58:05 +0700

    [GREEN] Implement Payment model to pass tests

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import java.util.Map;
+
+@Getter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+    }
+
+    public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException("Status tidak valid");
+        }
+    }
+}
